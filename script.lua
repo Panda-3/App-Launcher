f87b081731aebe172b47b4bdde523717e8aaebdf
vampirexhunter <ninja0tester@gmail.com>
2017-04-26 08:54:22 -0400

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,88 @@
+-- App Launcher BETA v0.1 --
+color.loadpalette()
+color.shine = color.new(255,255,255,100) -- new color shine!
+back = image.load("back.png")
+logo = image.load("sce_sys/icon0.png")
+over = 1
+buttons.interval(8,8)
+buttons.analogtodpad(60)
+xtitle = 240
+local pos = 1;
+local result,result_rmv = 0,0
+local icons = {}
+local list = {data = {}, len = 0}
+function reload_list()
+	pos = 1;
+	icons = {}
+	list.data = game.list()
+	table.sort(list.data ,function (a,b) return string.lower(a.id)<string.lower(b.id); end)
+	list.len = #list.data
+	for i=1,list.len do
+		if list.data[i].path:sub(1,4) == "ux0:" then
+			if hw.removablemc() == 1 then
+				list.data[i].location = "Mem-Card"
+			else
+				list.data[i].location = "Mem-Emu"
+			end
+			list.data[i].flag = 1
+		else
+			list.data[i].location = "Internal"
+			list.data[i].flag = 0
+		end
+		local img = nil
+		local info = nil
+		if list.data[i].path:sub(1,10) == "ux0:pspemu" then
+			img = game.geticon0(string.format("%s/eboot.pbp",list.data[i].path))
+			info = game.info(string.format("%s/eboot.pbp",list.data[i].path))
+		else
+			img = image.load(string.format("%s/sce_sys/icon0.png",list.data[i].path))
+			info = game.info(string.format("%s/sce_sys/param.sfo",list.data[i].path))
+		end
+		if info and info.TITLE then
+			list.data[i].title = info.TITLE
+		end
+		icons[i] = img
+	end
+end
+reload_list()
+while true do
+	buttons.read()
+	if back then back:blit(0,0) end
+	screen.print(480,10,"Game Launcher (Apps - Bubbles)",1,color.white,color.blue,__ACENTER)
+	screen.print(950,10,os.date("%I:%M %p"),1,color.white,0x0,__ARIGHT)
+	screen.print(150,10,"Battery " + batt.lifepercent () + "%",1,color.white,0x0,__ARIGHT)
+	screen.print(950,30,"Apps: " + list.len,1,color.red,0x0,__ARIGHT)
+	if list.len > 0 then
+		if buttons.up and pos > 1 then pos -= 1 end
+		if buttons.down and pos < list.len then pos += 1 end
+		if buttons.cross then
+			game.launch(list.data[pos].id)
+		end
+		if icons[pos] then
+			screen.clip(950-64,405+64, 128/2)
+			icons[pos]:center()
+			icons[pos]:blit(950-128 + 64,405 + 64)
+			screen.clip()
+		else
+			draw.fillrect(950-128,35, 128, 128, color.white:a(100))
+			draw.rect(950-128,35, 128, 128, color.white)
+		end
+		local y = 75
+		for i=pos,math.min(list.len,pos+15) do
+			if i == pos then
+				screen.print(10,y,"->")
+			end
+			screen.print(40,y,'#'+string.format("%03d",i)+' '+list.data[i].title or "unk",1,color.white,color.orange)
+			screen.print(800,y,list.data[i].id or "unk",1,color.white,color.yellow)
+			--screen.print(600,y,list[i].cat or "unk")
+			y += 20
+		end
+		screen.print(10,495,"Press X to Launch " + list.data[pos].title,1,color.magenta)
+	else
+		screen.print(10,30,"Vacio o error?")
+	end
+
+	screen.flip()
+
+	if buttons.released.start then break end
+end
